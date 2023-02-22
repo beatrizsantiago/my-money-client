27e732f398a69665070e9f29d5126f6b85ec7782
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2023-02-22 19:37:26 -0300

    🎉 initial setup

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,8 +2,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="pt-Br">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;600;700&display=swap" rel="stylesheet" />
+      </Head>
       <body>
         <Main />
         <NextScript />
